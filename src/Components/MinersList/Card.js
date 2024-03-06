67c43aaf0329bb08280322b4e07186125069f4f7
Zhiyu Zhou <zhouhoushu@126.com>
2024-03-06 12:05:44 +0800

    Add styling

--- src/Components/MinersList/Card.js
+++ src/Components/MinersList/Card.js
@@ -1,7 +1,10 @@
+import { States } from "../../constants";
+
 export const Card = ({
     planet,
     name,
     status,
+    minerals,
     carryCapacity,
     x,
     y,
@@ -12,10 +15,19 @@ export const Card = ({
             <div class="left">
                 <div class="top">{planet}</div>
                 <div class="middle">{name}</div>
-                <div class="bottom">{status}</div>
+                <span class="bottom">{States[status]}</span>
             </div>
-            <div class="center"></div>
-            <div class="right"></div>
+            <div class="center">
+                <div class="capacity-name">Carry Capacity</div>
+                <div class="capacity-content" style={{ color: minerals < carryCapacity ? 'rgba(255, 255, 255, 1)' : 'rgba(107, 228, 128, 1)' }}>{minerals}/{carryCapacity}</div>
+                <div class="position-name">Position</div>
+                <div class="position-content">{Math.floor(x)},{Math.floor(y)}</div>
+            </div>
+            <div class="right">
+                <div class="speed-name">Travel Speed</div>
+                <div class="speed-content">{travelSpeed}</div>
+            </div>
+            <img src={require("../../Images/arrow.png")} class="arrow" />
         </div>
     );
 };
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const States = {
+    0: "Idle",
+    1: "Traveling",
+    2: "Mining",
+    3: "Transferring"
+};
